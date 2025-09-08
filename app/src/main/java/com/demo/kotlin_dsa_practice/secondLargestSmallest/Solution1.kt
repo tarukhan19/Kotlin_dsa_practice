97dd05243bfe397b3d279c1580d1dbd93c79085e
taru <tarukhan19@gmail.com>
2025-09-08 17:06:05 +0530

    missing number

---
+++ app/src/main/java/com/demo/kotlin_dsa_practice/secondLargestSmallest/Solution1.kt
@@ -0,0 +1,57 @@
+package com.demo.kotlin_dsa_practice.secondLargestSmallest
+
+fun main() {
+    var arr = intArrayOf(1, 4, 3, 16, 8, 12)
+    var maxNum = findSecondSmallest(arr)
+    println(maxNum)
+}
+
+// brute force
+fun findSecondLargeSmall(arr: IntArray): Pair<Int, Int> {
+    for (i in 0..arr.size - 1) {
+        for (j in i + 1..arr.size - 1) {
+            if (arr[i] > arr[j]) {
+                var temp = arr[j]
+                arr[j] = arr[i]
+                arr[i] = temp
+            }
+        }
+    }
+
+    var n = arr.size
+    var secondLargest = arr[n - 2]
+    var secondSmallest = arr[1]
+    return Pair(secondLargest, secondSmallest)
+}
+
+fun findSecondSmallest(array: IntArray): Pair<Int, Int> {
+    var smallestNo = Int.MAX_VALUE
+    var secSmallestNo = Int.MAX_VALUE
+
+    for (i in 0 until array.size) {
+        if (array[i] < smallestNo) {
+            secSmallestNo = smallestNo
+            smallestNo = array[i]
+
+        } else if (array[i] > smallestNo &&  array[i] < secSmallestNo) {
+            secSmallestNo = array[i]
+        }
+    }
+    return Pair(smallestNo, secSmallestNo)
+}
+
+fun findSecondLargestOptimize(arr: IntArray): Pair<Int, Int> {
+    var largest = Int.MIN_VALUE
+    var secLargest = Int.MIN_VALUE
+
+    for (i in 0 until arr.size) {
+        if (arr[i] > largest) {
+            secLargest = largest
+            largest = arr[i]
+        } else if (arr[i] < largest && arr[i] > secLargest) {
+            secLargest = arr[i]
+        }
+    }
+
+    return Pair(largest, secLargest)
+}
